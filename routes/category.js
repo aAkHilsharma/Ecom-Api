3727ef9af56946c426e0dd9bfc066d3e28b5bc1f
aAkHilsharma <akhilsharma6721@gmail.com>
2023-08-18 08:49:41 -0700

    added category routes

--- routes/category.js
+++ routes/category.js
@@ -1,3 +1,71 @@
 const router = require("express").Router();
+const Category = require("../models/category");
+
+// route - /api/category
+// method - get
+// desc - get all the categories
+
+router.get("/", async (req, res) => {
+  try {
+    const categories = await Category.find({}, "name");
+    res
+      .status(200)
+      .json({
+        success: true,
+        message: "Categories fetched successfully",
+        categories,
+      });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Error retrieving categories",
+        error: error.message,
+      });
+  }
+});
+
+// route - api/category
+//method - post
+// desc - Create a new category
+
+router.post("/", async (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Name is required" });
+  }
+
+  try {
+    const existingCategory = await Category.findOne({ name });
+
+    if (existingCategory) {
+      return res
+        .status(409)
+        .json({
+          success: false,
+          message: "Category with the same name already exists",
+        });
+    }
+
+    const category = new Category({ name });
+    await category.save();
+    res.status(201).json({
+      success: true,
+      message: "Category added successfully",
+      category,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Error creating category",
+        error: error.message,
+      });
+  }
+});
 
 module.exports = router;
