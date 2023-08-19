26603b4900676be728b714b7d1d4af035e800530
aAkHilsharma <akhilsharma6721@gmail.com>
2023-08-18 22:14:52 -0700

    added rate limiter to all routes

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
-const express = require('express');
-require('dotenv').config();
-require('./config/dbConfig');
+const express = require("express");
+require("dotenv").config();
+require("./config/dbConfig");
 const app = express();
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again later.",
+});
 
 app.use(express.json());
+app.use(limiter);
 
-app.use('/api/users', require('./routes/auth'));
+app.use("/api/users", require("./routes/auth"));
 app.use("/api/categories", require("./routes/category"));
-app.use('/api/products', require('./routes/product'));
-app.use('/api/cart', require('./routes/cart'));
+app.use("/api/products", require("./routes/product"));
+app.use("/api/cart", require("./routes/cart"));
 app.use("/api/orders", require("./routes/order"));
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-})
+  console.log(`Server listening on port ${port}`);
+});
