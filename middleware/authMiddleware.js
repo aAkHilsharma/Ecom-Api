77b8daf1a9cba330a3041898c9acce24f4d581da
aAkHilsharma <akhilsharma6721@gmail.com>
2023-08-18 21:08:44 -0700

    added error checks in middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,13 +3,20 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Authentication token is missing",
+      });
+    }
+
     const decryptedToken = jwt.verify(token, process.env.jwt_secret);
     req.user = decryptedToken.user;
     next();
   } catch (error) {
-    res.status(500).send({
+    res.status(401).json({
       success: false,
-      message: error.message,
+      message: "Authentication failed"
     });
   }
 };
